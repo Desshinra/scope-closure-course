55c0a99cd7c99677350ad523d21e6a554c15b333
desshinra <desshinra@gmail.com>
2023-04-04 14:33:51 -0600

    hoisting in Javascript

---
+++ src/hoisting/index.js
@@ -0,0 +1,13 @@
+// var petName; //undefined
+
+// console.log(petName);
+// let petName = 'Socrates';
+// console.log(petName);
+
+petName();
+
+var dog = 'Juan'; //If
+function petName() {
+    console.log(`El mejor perro es ${dog}`);
+}
+
